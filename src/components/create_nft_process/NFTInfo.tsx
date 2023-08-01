36621497143bdf39103a1cf988ea84d62aff7343
joy <chohee1231@gmail.com>
2023-08-01 21:13:55 +0900

    add image in mobile chat

--- src/components/create_nft_process/NFTInfo.tsx
+++ src/components/create_nft_process/NFTInfo.tsx
@@ -1,10 +1,16 @@
 import ChatMessageByAdmin from "@/components/create_nft_process/ChatMessageByAdmin";
+import Image from "next/image";
 
 const NFTInfo = () => {
   return (
     <>
       <ChatMessageByAdmin
         text={'지갑에 씨앗 NFT가 에어드랍 되었습니다.'}
+        childrenInBubble={
+        <div className={""}>
+        <div className={"w-[120px] h-[120px] relative mt-2"}>
+          <Image src={'/images/references/seed.png'} alt={'seed image'} fill className={"rounded-2xl"}/>
+        </div></div>}
       >
       </ChatMessageByAdmin>
       <ChatMessageByAdmin
